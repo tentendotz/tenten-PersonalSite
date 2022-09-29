b129cd4c8c60f3cddc159006d686cbb5ab00a9f1
tentendotz <tenten102matsuyama@gmail.com>
2022-09-29 23:44:25 +0900

    Optimize page loading speed to interact with a user

--- js/appearance.js
+++ js/appearance.js
@@ -1,57 +1,11 @@
 /* MARK: - Fade-in effect
 ----------------------------------------------------- */
-const fadeInTarget = document.querySelectorAll(".container-fluid");
+const fadeInTarget = document.querySelectorAll(".disappeared");
 
-hidden();
 window.addEventListener("load", fadeIn);
 
-function hidden() {
-  if (!fadeInTarget) return;
-  for (let i = 0; i < fadeInTarget.length; i++) {
-    fadeInTarget[i].classList.add("disappeared");
-  }
-}
-
 function fadeIn() {
-  if (!fadeInTarget) return;
   for (let i = 0; i < fadeInTarget.length; i++) {
     fadeInTarget[i].classList.add("show-off");
   }
 }
-
-
-/* MARK: - Twitter Script
------------------------------------------------------ */
-window.twttr = (function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
-
-  t._e = [];
-  t.ready = function (f) {
-    t._e.push(f);
-  };
-
-  return t;
-})(document, "script", "twitter-wjs");
-
-// For Twitter Timeline
-const tweetContent = document.querySelector(".tweet-user-timeline");
-twttr.widgets.createTimeline(
-  {
-    sourceType: "profile",
-    screenName: "tentenblog",
-  },
-  tweetContent,
-  {
-    theme: "light",
-    width: 300,
-    tweetLimit: 2,
-    chrome: "nofooter",
-  }
-);
---
+++ js/twitter.js
@@ -0,0 +1,35 @@
+/* MARK: - Twitter Script
+----------------------------------------------------- */
+window.twttr = (function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function (f) {
+    t._e.push(f);
+  };
+
+  return t;
+})(document, "script", "twitter-wjs");
+
+// For Twitter Timeline
+const tweetContent = document.querySelector(".tweet-user-timeline");
+twttr.widgets.createTimeline(
+  {
+    sourceType: "profile",
+    screenName: "tentenblog",
+  },
+  tweetContent,
+  {
+    theme: "light",
+    width: 300,
+    tweetLimit: 2,
+    chrome: "nofooter",
+  }
+);
