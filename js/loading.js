242db772ff1b43c2209e9898f7fb195dfe88fc53
tentendotz <tenten102matsuyama@gmail.com>
2024-01-13 23:24:07 +0900

    Add loading animation for hero section

--- js/loading.js
+++ js/loading.js
@@ -1,10 +1,12 @@
-/* MARK: - Fade-in effect
+/* MARK: - Loader Animation for Fade-in effect
 ----------------------------------------------------- */
+const pageLoader = document.querySelector(".loading-container");
 const fadeInTarget = document.querySelectorAll(".disappeared");
 
 window.addEventListener("load", fadeIn);
 
 function fadeIn() {
+  pageLoader.style.display = "none";
   for (let i = 0; i < fadeInTarget.length; i++) {
     fadeInTarget[i].classList.add("show-off");
   }
